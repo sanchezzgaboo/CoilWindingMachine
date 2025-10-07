b011704bdd715dd9ac83d83d8fda1e5d1d8f5041
SisasCamel <gsanchezv07@outlook.es>
2025-10-07 10:23:57 -0500

    added platformio

---
+++ .pio/build/featheresp32/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/featheresp32/FrameworkArduino/cbuf.cpp.o: \
+ C:/Users/gsanc/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ C:/Users/gsanc/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
---
+++ .pio/build/featheresp32/FrameworkArduino/firmware_msc_fat.c.d
@@ -0,0 +1,3 @@
+.pio/build/featheresp32/FrameworkArduino/firmware_msc_fat.c.o: \
+ C:/Users/gsanc/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
+ C:/Users/gsanc/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
